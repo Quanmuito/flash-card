eb28f5d5a857e14f3b6974ae80a5e41713ce4d09
quanmuito <tranleminhquan1102@gmail.com>
2026-02-06 15:15:43 +0200

    fix translation

--- src/libs/utils.tsx
+++ src/libs/utils.tsx
@@ -1,7 +1,9 @@
 import verbitData from '../data/verbit.json';
+import verbit2Data from '../data/verbit2.json';
 import { Word } from '../types';
 
 export function getListOfWords(): Word[] {
-    const shuffled = [...verbitData].sort(() => Math.random() - 0.5);
+    const merged = [...verbitData, ...verbit2Data];
+    const shuffled = merged.sort(() => Math.random() - 0.5);
     return shuffled.slice(0, 10);
 }
